f514e22c7e10f2adac9e0bdf428a1ee53cf6ced3
Greg Wilwerding <greg.wilwerding@gmail.com>
2017-05-09 12:15:03 -0500

    'added if/else'

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne, cardTwo);
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+} 
