a8693d53a1a0018b47272a7e2e2b49e087ab2b0e
Greg Wilwerding <greg.wilwerding@gmail.com>
2017-05-09 15:53:46 -0500

    'add array objects'

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+  	"rank": "queen",
+  	"suit": "hearts",
+  	"cardImage": "images/queen-of-hearts.png"
+  },
+    {
+  	"rank": "queen",
+  	"suit": "diamonds",
+  	"cardImage": "images/queen-of-diamonds.png"
+  },
+    {
+  	"rank": "king",
+  	"suit": "hearts",
+  	"cardImage": "images/king-of-hearts.png"
+  },
+    {
+  	"rank": "king",
+  	"suit": "diamonds",
+  	"cardImage": "images/king-of-diamonds.png"
+  },
+
+
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
@@ -9,9 +32,9 @@ var checkForMatch = function() {
   }
 }
 
-var flipCard = function(cards){
-  console.log("User flipped " + cards[0]);
-  console.log("User flipped " + cards[2]);
+var flipCard = function(){
+  console.log("User flipped " + cards[0].rank + " " + cards[0].suit);
+  console.log("User flipped " + cards[1].rank + " " + cards[1].suit);
   cardsInPlay.push[cards];
   if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
