a10a7dcbe3260f8174a24f0bb9c9e0c2c70e1534
Greg Wilwerding <greg.wilwerding@gmail.com>
2017-05-09 12:34:49 -0500

    Add files via upload
    'added functions'

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-cardsInPlay.push(cardOne, cardTwo);
+var checkForMatch = function() {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+  	console.log("You found a match!");
+  } else {
+  	console.log("Sorry, try again.");
+  }
+}
 
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
+var flipCard = function(cards){
+  console.log("User flipped " + cards[0]);
+  console.log("User flipped " + cards[2]);
+  cardsInPlay.push[cards];
+  if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
-} 
+  } 
+};
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
